4ea2202de14e61bbbbc7f0120196605eb216f18e
Eike Buchheister <eike27@outlook.de>
2019-08-30 15:06:39 +0200

    added wakeUpTest

---
+++ src/assignment3/election/test/WakeUpTest.java
@@ -0,0 +1,54 @@
+package assignment3.election.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+
+import assignment3.election.SimpleElectionNode;
+
+class WakeUpTest {
+  
+  private SimpleElectionNode a;
+  private SimpleElectionNode b;
+  private SimpleElectionNode c;
+  private SimpleElectionNode d;
+  
+  @BeforeEach
+  void setUp() throws Exception {
+    a = new SimpleElectionNode("A", 3, false);
+    b = new SimpleElectionNode("B", 2, true);
+    c = new SimpleElectionNode("C", 1, false);
+    d = new SimpleElectionNode("D", 4, true);
+    a.setupNeighbours(b, c, d);
+    b.setupNeighbours(a, c, d);
+    c.setupNeighbours(a, b, d);
+    d.setupNeighbours(a, b, c);
+  }
+
+  @AfterEach
+  void tearDown() throws Exception {
+  }
+
+  @Test
+  @RepeatedTest(value = 99)
+  void test() {
+    assertEquals(Integer.MIN_VALUE, c.getStrongestIdentity());
+    
+    c.wakeup(a, 3);
+    assertEquals(a.getIdentity(), c.getStrongestIdentity());
+    assertEquals(a, c.getWokeUpBy());
+    
+    c.wakeup(b, 2);
+    assertEquals(a.getIdentity(), c.getStrongestIdentity());
+    assertEquals(a, c.getWokeUpBy());
+
+    c.wakeup(d, 4);
+    assertEquals(d.getIdentity(), c.getStrongestIdentity());
+    assertEquals(d, c.getWokeUpBy());
+  
+  }
+
+}
